61d8d0bc77da9e9e5e6d8abf10b7275c4aece4e9
vaibhavbuccha <jainvaibhav415@gmail.com>
2021-03-28 18:58:12 +0530

    Recipes App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import React, { useState } from 'react';
-import Tweet from './tweet';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
+import Recipe from './Recipe';
 
 function App(){
-  const [users, setUsers] = useState([
-    {name: "Vaibhav", message: "I am a developer"},
-    {name: "John snow", message: "I'm a great king"},
-    {name: "Winter is comming", message: "This is from GOT session 1."}
-  ])
-  
+  const APP_ID = "e27033e9";
+  const APP_KEY = "afa00c7021b947bbe37650ad4bc27d10";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('indian');
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+  }
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+  }
+
   return(
-    <div className="app" >
-      {
-        users.map((user) => (
-          <Tweet name={user.name} message={user.message} />
-        ))
-      }
+    <div className="App" >
+      <form className="search-form" onSubmit={getSearch} >
+        <input className="search-bar" value={search} type="text" onChange={updateSearch} />
+        <button  className="search-button" >
+          Search
+        </button> 
+      </form>
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe 
+            title={recipe.recipe.label} 
+            calories={recipe.recipe.calories} 
+            image={recipe.recipe.image} 
+            ingredients={recipe.recipe.ingredients}
+          />
+        ) )}
+      </div>
     </div>
   )
 }
