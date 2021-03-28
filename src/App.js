af35d1f63537efc472514a67a346bd41df7de220
vaibhavbuccha <jainvaibhav415@gmail.com>
2021-03-28 16:49:05 +0530

    Basics of react like components usestate and props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Tweet from './tweet';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+  const [users, setUsers] = useState([
+    {name: "Vaibhav", message: "I am a developer"},
+    {name: "John snow", message: "I'm a great king"},
+    {name: "Winter is comming", message: "This is from GOT session 1."}
+  ])
+  
+  return(
+    <div className="app" >
+      {
+        users.map((user) => (
+          <Tweet name={user.name} message={user.message} />
+        ))
+      }
     </div>
-  );
+  )
 }
+
 
 export default App;
